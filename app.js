3bafe813fb17d8734c1e2b358930c21682fee52f
Sardorbek <stozaboev@gmail.com>
2021-04-02 22:21:33 +0500

    todo list version 1

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const express=require("express");
+
+const bodyParser=require("body-parser");
+
+const app=express();
+
+let items=["Buy Food","Cook Food","Eat Food"];
+let workItems=[];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/", function(req,res){
+    let today=new Date();
+     let options={
+         weekday:"long",
+         day:"numeric",
+         month:"long",
+
+     };
+    
+    let day=today.toLocaleDateString("en-US",options);
+
+    res.render("list", {listTitle:day, newListItems:items});
+    
+})
+
+app.post("/",function(req,res){
+    
+    let item=req.body.newItem;
+
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        
+   items.push(item);
+    
+   res.redirect("/");
+    }
+})
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle:"Work list",newListItems:workItems})
+})
+app.post("/work", function(req,res){
+    let item=req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/about", function(req,res){
+    res.render("about");
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
